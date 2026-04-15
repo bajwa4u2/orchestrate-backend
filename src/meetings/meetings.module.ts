7953caa5800baf08b7b9ab28ea85729b5e2decbb
bajwa4u2 <muhammadsakhawat@gmail.com>
2026-04-15 10:54:32 -0400

    compaign activation 1

--- src/meetings/meetings.module.ts
+++ src/meetings/meetings.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { AccessContextModule } from '../access-context/access-context.module';
+
 import { MeetingsController } from './meetings.controller';
 import { MeetingsService } from './meetings.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    AccessContextModule, // ✅ REQUIRED FIX
+  ],
   controllers: [MeetingsController],
   providers: [MeetingsService],
   exports: [MeetingsService],
