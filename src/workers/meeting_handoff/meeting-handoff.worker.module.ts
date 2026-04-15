65bf242da11e0e889009095ed5ed2b90b9677adc
bajwa4u2 <muhammadsakhawat@gmail.com>
2026-04-14 23:16:16 -0400

    worker assignment deploy error fix2

--- src/workers/meeting_handoff/meeting-handoff.worker.module.ts
+++ src/workers/meeting_handoff/meeting-handoff.worker.module.ts
@@ -1,10 +1,10 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../../database/database.module';
 import { EmailsModule } from '../../emails/emails.module';
 import { MeetingHandoffWorkerService } from './meeting-handoff.worker.service';
 
 @Module({
-  imports: [DatabaseModule, forwardRef(() => EmailsModule)],
+  imports: [DatabaseModule, EmailsModule],
   providers: [MeetingHandoffWorkerService],
   exports: [MeetingHandoffWorkerService],
 })
