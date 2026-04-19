d78f4e6293d5aa8bac2bc0a81d758b095638bbfb
bajwa4u2 <muhammadsakhawat@gmail.com>
2026-04-19 15:46:36 -0400

    operator services module re-aligned

--- src/operator/operator.module.ts
+++ src/operator/operator.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { AccessContextModule } from '../access-context/access-context.module';
 import { BillingModule } from '../billing/billing.module';
 import { ControlModule } from '../control/control.module';
+import { ClientsModule } from '../clients/clients.module';
+import { CampaignsModule } from '../campaigns/campaigns.module';
+import { EmailsModule } from '../emails/emails.module';
+import { DeliverabilityModule } from '../deliverability/deliverability.module';
 import { OperatorController } from './operator.controller';
 import { OperatorService } from './operator.service';
 
 @Module({
-  imports: [AccessContextModule, BillingModule, ControlModule],
+  imports: [
+    AccessContextModule,
+    BillingModule,
+    ControlModule,
+    ClientsModule,
+    CampaignsModule,
+    EmailsModule,
+    DeliverabilityModule,
+  ],
   controllers: [OperatorController],
   providers: [OperatorService],
 })
