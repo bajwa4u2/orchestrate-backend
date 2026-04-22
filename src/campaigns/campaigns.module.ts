c98ba9233951d5026c40ef732d8e991c2a117d17
bajwa4u2 <muhammadsakhawat@gmail.com>
2026-04-22 10:42:18 -0400

    fix campaigns deliverability module wiring

--- src/campaigns/campaigns.module.ts
+++ src/campaigns/campaigns.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AccessContextModule } from '../access-context/access-context.module';
 import { DatabaseModule } from '../database/database.module';
+import { DeliverabilityModule } from '../deliverability/deliverability.module';
 import { WorkersModule } from '../workers/workers.module';
 import { WorkflowsModule } from '../workflows/workflows.module';
 import { CampaignsController } from './campaigns.controller';
 import { CampaignsService } from './campaigns.service';
 
 @Module({
-  imports: [DatabaseModule, WorkflowsModule, AccessContextModule, WorkersModule],
+  imports: [
+    DatabaseModule,
+    WorkflowsModule,
+    AccessContextModule,
+    WorkersModule,
+    DeliverabilityModule,
+  ],
   controllers: [CampaignsController],
   providers: [CampaignsService],
   exports: [CampaignsService],
